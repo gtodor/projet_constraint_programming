7adf9fb71313537d78494b3d91cb630c742063bc
AndreiTodor <bvandrei94@gmail.com>
2017-02-09 22:36:06 +0100

    add header domaine.hpp and domaine.cpp

--- domaine.cpp
+++ domaine.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 #include "domaine.hpp"
+
+using namespace std;
+
+domaine::domaine(int size): list_val(size,0){
+  
+}
+
+
+domaine::domaine(const domaine& d){
+
+}
+
+domaine::~domaine(){
+
+}
--- domaine.hpp
+++ domaine.hpp
@@ -1,11 +1,22 @@
 #ifndef domaine_hpp
 #define domaine_hpp
 
-class domaine{
+#include <vector>
 
+class domaine{
+private:
+  std::vector<int> list_val;
+
+public:
+  domaine(int size);
+  domaine(const domaine& d);
+  ~domaine();
+
+  const int& operator[](int index) const; //array like behaviour
+  int get_size();
+  void set_val(int index);
+  void set_all_val(std::vector<int> v);//maybe not necessarry
 
 };
-
-
 
 #endif //domaine_hpp

e8a925d589131efca0a196eed36a29cf607daadd
AndreiTodor <bvandrei94@gmail.com>
2017-02-09 22:37:53 +0100

    add methods to solver hpp

--- solver.hpp
+++ solver.hpp
@@ -1,8 +1,32 @@
 #ifndef solver_hpp
 #define solver_hpp
 
-class solver{
+#include <string>
+#include "prune.hpp"
+#include "problem.hpp"
 
+class solver{
+private:
+  prune* prune_algo;//what prune algoritm we want to use
+  problem* p;// reference to the problem
+
+public:
+  solver(problem* p);
+  solver(problem* p, std::string prune_algo);
+  ~solver();
+
+  //return implemented prune algorithm names 
+  std::string get_prune_algos();
+
+  //resolvs the problem by branch and prune algorithm
+  void solve();
+
+  //finds the next solution from current state  
+  void step_solve();
+
+  //print the solutions of the problem
+  void show_solutions();
+  
 };
 
 #endif // solver_hpp

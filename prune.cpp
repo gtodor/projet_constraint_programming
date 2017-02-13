09317159557dabed9005637013f475754963fd96
AndreiTodor <bvandrei94@gmail.com>
2017-02-14 00:42:37 +0100

    addcomments to prune.hpp

--- prune.cpp
+++ prune.cpp
@@ -22,15 +22,12 @@ void prune::simple_prune(node& n){
       break;
     }
   }
-  //cout<<"all1? : "<<all1<<endl;
   for(unsigned int i=0; i<n.nb_vars(); i++){
     if(n[i].size() == 0){
       domaine_null=true;
       break;
     }
   }
-  //cout<<"domaine null? :"<<domaine_null<<endl;
-  //cout<<"sat? : "<<sat<<endl;
   if(all1 && sat){
     n.set_solution(true);
   }
--- prune.hpp
+++ prune.hpp
@@ -4,6 +4,9 @@
 #include "problem.hpp"
 #include "node.hpp"
 
+/*
+ *class that contains prune methods for use in branchAndPrune algorithm 
+ */
 class prune{
 private:
   problem* p;
@@ -11,6 +14,12 @@ private:
 public:
   prune(problem* p);
   ~prune();
+
+  /*
+   *method that only checks the constraints
+   *param[in] reference to a node
+   *if the constraints are satisfied and all domains size equal 1 this method sets the variable solution in node to true; else it empties the node
+   */
   void simple_prune(node& n);  
   
 };

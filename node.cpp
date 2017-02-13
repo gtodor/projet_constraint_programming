5c7942203ca017f7104fb8a999815932bcc12fc6
AndreiTodor <bvandrei94@gmail.com>
2017-02-14 00:33:31 +0100

    added comments to node.hpp

--- node.cpp
+++ node.cpp
@@ -9,12 +9,10 @@ node::node(int nr_vars) : domaines(nr_vars,domaine(0)), solution(false){
 }
 
 node::node(const node& n) : domaines(n.domaines){
-  //cout<<"inside node copy constructor"<<endl;
   solution = n.solution;
 }
 
 node& node::operator=(node& other){
-  //cout<<"inside node::operator="<<endl;
   if(this == &other) return *this;
   domaines.erase(domaines.begin(), domaines.end());
   for(unsigned int i=0; i<other.nb_vars(); i++){
@@ -24,7 +22,6 @@ node& node::operator=(node& other){
 }
 
 domaine& node::operator[](unsigned int index){
-  //cout<<"inside node::operator[] "<<endl;
   if(index >= domaines.size()){
     throw std::invalid_argument("index out of bounds");
   }
@@ -74,7 +71,7 @@ unsigned int node::smallest_domaine(){
     }
   }
   if(!found){
-    
+    return -2;
   }
   for(unsigned int i=0; i<nb_vars(); i++){
     if(domaines[i].size() > 1 && domaines[i].size() < domaines[min].size()) min = i;
--- node.hpp
+++ node.hpp
@@ -4,22 +4,62 @@
 #include <vector>
 #include "domaine.hpp"
 
+/*
+ *class that is used to store informations about the current state of a problem
+ */
 class node{
 private:
   std::vector<domaine> domaines;
   bool solution;
 public:
+  /*
+   *param[in] the number of varibles of the problem
+   *at initialization the node contains nr_vars empty domains
+   */
   node(int nr_vars);
-  node(const node& n);
 
+  node(const node& n);
+  /*
+   *assignement operator used to copy one node to another
+   */
   node& operator=(node& other);
+  /*
+   * we can acess each domain of the node like an array
+   * ex: node n(3); n[1] represents the 2nd domain of the node
+   */
   domaine& operator[](unsigned int index);
+  /*
+   *return number of variables/domains in the node
+   */
   unsigned int nb_vars();
+
+  /*
+   *prints a graphic representation of a node
+   */ 
   void print_node();
+
+  /*
+   *return true if every domain in node is empty
+   */
   bool is_empty();
+  /*
+   *return true if this node corresponds to a solution of the problem
+   */
   bool is_solution();
+  /*
+   *sets the variable solution at true if this node contain a solution
+   */
   void set_solution(bool var);
+
+  /*
+   *return the index of the smallest domain in the node that has not a size of 1
+   *if there is no such index (all domains have a size of 1) it returns a negative number irrelevant to the problem
+   */
   unsigned int smallest_domaine();
+
+  /*
+   *return domaines
+   */
   std::vector<domaine> get_domaines();
   
 };

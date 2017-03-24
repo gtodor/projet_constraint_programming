0a62910cd99c375b45fc9255755a12837c6a3a33
AndreiTodor <bvandrei94@gmail.com>
2017-03-24 13:38:01 +0100

    modif du solver appelle en main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "queens.hpp"
 #include "solver.hpp"
 #include "forward_checking.hpp"
+#include "arcConsistency.hpp"
 
 using namespace std;
 
@@ -16,6 +17,7 @@ int main(){
   problem* p = new queens(n);
   //solver* s = new solver(p); // default prune algorithm -- backtracking
   solver* s = new solver(p,new forward_checking(p)); // prune algorithm fwd chk
+  //solver* s = new solver(p, new arcConsistency(p));
   s->solve();
   s->show_solutions();
   
